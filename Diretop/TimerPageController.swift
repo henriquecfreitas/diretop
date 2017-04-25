8550c141846c43110ea22b6dd8748fc0bc26c9b4
Henrique <henrique@MacBook-Air-de-Henrique.local>
2017-04-25 01:02:51 -0300

    Creating a class to store the alarm settings and execute the alarm functions

--- Diretop/TimerPageController.swift
+++ Diretop/TimerPageController.swift
@@ -7,13 +7,18 @@
 //
 
 import UIKit
+import AudioToolbox
 
 class TimerPageController: UIViewController {
     //VARS
+    @IBOutlet var timerPageView: UIView!
     @IBOutlet weak var timerLabel: UILabel!
     let appDelegate = UIApplication.shared.delegate as! AppDelegate
     var speechTime = 0
-    var timeLeft = 0.0
+    var timeLeft: Double = 0.0
+    
+    var timerAlertConfig = TimerAlertConfig()
+    
     var timer = Timer()
     
     @IBOutlet weak var timerNavBar: UINavigationItem!
@@ -37,6 +42,8 @@ class TimerPageController: UIViewController {
         pauseButton = UIBarButtonItem(barButtonSystemItem: .pause, target: self, action: #selector(TimerPageController.timerPause(_:)))
         enableStartButton()
         progressBar.progress = 0.0
+        
+        timerAlertConfig = TimerAlertConfig(pageView: timerPageView, secondsLeft: 5.0, totalBlinks: 3, duration: 2.0, alreadyCalled: false)
     }
     
     override func didReceiveMemoryWarning() {
@@ -50,10 +57,14 @@ class TimerPageController: UIViewController {
         updateLabel()
         updateProgressBar()
         
+        if (timeLeft <= timerAlertConfig.secondsLeft && timerAlertConfig.alreadyCalled == false) {
+            timerAlertConfig.blinkScreen()
+            timerAlertConfig.alreadyCalled = true
+        }
+        
         if(timeLeft <= 0) {
             timer.invalidate()
             enableStartButton()
-            
         }
     }
     
@@ -84,7 +95,7 @@ class TimerPageController: UIViewController {
     
     @IBAction func timerStart(_ sender: Any) {
         if (timeLeft <= 0) {
-            timeLeft = Double(speechTime)
+            resetTimerInfo()
         }
         
         if (timer.isValid) {
@@ -97,19 +108,74 @@ class TimerPageController: UIViewController {
     }
     
     @IBAction func timerRestart(_ sender: Any) {
-        timer.invalidate()
+        resetTimerInfo()
+        stopTimerCountdown()
+    }
+    
+    @IBAction func timerPause(_ sender: Any) {
+        stopTimerCountdown()
+    }
+    
+    func resetTimerInfo() {
+        timerAlertConfig.alreadyCalled = false
         timeLeft = Double(speechTime)
         
         updateLabel()
         updateProgressBar()
-        
-        enableStartButton()
     }
     
-    @IBAction func timerPause(_ sender: Any) {
-        if (timer.isValid) {
-            timer.invalidate()
-        }
+    func stopTimerCountdown() {
+        timer.invalidate()
         enableStartButton()
+    }
+}
+
+class TimerAlertConfig {
+    var pageView = UIView().self
+    
+    var secondsLeft: Double = 0.0
+    var totalBlinks: Int = 0
+    var duration: Double = 0.0
+    var alreadyCalled: Bool = false
+    
+    public init(){
+    }
+    
+    public init(pageView: UIView, secondsLeft: Double, totalBlinks: Int, duration: Double, alreadyCalled: Bool) {
+        self.pageView = pageView
+        self.secondsLeft = secondsLeft
+        self.totalBlinks = totalBlinks
+        self.duration = duration
+        self.alreadyCalled = alreadyCalled
+    }
+    
+    public func paintScreen(backgroundColor: UIColor){
+        self.pageView.backgroundColor = backgroundColor
+    }
+    
+    public func blinkScreen() {
+        var doneBlinks = 0
+        
+        let redColor = UIColor(red: 1, green: 0, blue: 0, alpha: 1)
+        let whiteColor = UIColor(white: 1, alpha: 1)
+        let halfBlinkDuration = duration / Double(totalBlinks) / 2.0
+        
+        func doBlink(){
+            AudioServicesPlayAlertSound(SystemSoundID(kSystemSoundID_Vibrate))
+            UIView.animate(withDuration: halfBlinkDuration, animations: { () -> Void in
+                self.paintScreen(backgroundColor: redColor)
+            }, completion: { (true) -> Void in
+                UIView.animate(withDuration: halfBlinkDuration, animations: { () -> Void in
+                    self.paintScreen(backgroundColor: whiteColor)
+                }, completion: { (true) -> Void in
+                    doneBlinks = doneBlinks + 1
+                    if (doneBlinks < self.totalBlinks) {
+                        doBlink()
+                    }
+                })
+            })
+        }
+        
+        doBlink()
     }
 }
